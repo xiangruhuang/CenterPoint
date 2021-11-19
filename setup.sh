890e19671419228948eb9ced18ad8c03e53c8621
xiangruhuang <xiangruhuang816@gmail.com>
2021-11-19 17:26:58 -0500

    modify setup shell

--- setup.sh
+++ setup.sh
@@ -3,3 +3,10 @@ python setup.py build_ext --inplace
 
 cd .. && cd  iou3d_nms
 python setup.py build_ext --inplace
+
+cd .. && cd  primitives
+python setup.py build_ext --inplace
+
+cd .. && cd .. && cd ../PytorchHashmap/torch_hash
+python setup.py build_ext --inplace
+

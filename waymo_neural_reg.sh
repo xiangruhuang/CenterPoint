8ff6d8a17a9310d18fd28c983b7a09e4cf4c38a2
xiangruhuang <xiangruhuang816@gmail.com>
2021-11-21 23:42:17 +0000

    update script

--- waymo_neural_reg.sh
+++ waymo_neural_reg.sh
@@ -2,8 +2,9 @@
 start=$1
 end=$2
 mkdir -p work_dirs/motion_estimation/
+mkdir -p logs
 
 for i in `seq ${start} ${end}`; do
   gpu_id=$((i % 8));
-  CUDA_VISIBLE_DEVICES=${gpu_id} python tools/parallel_data_iterator.py configs/waymo/motion_mask_compuation/waymo_neural_reg.py --split ${i} > ${i}.log 2>&1 &
+  CUDA_VISIBLE_DEVICES=${gpu_id} python tools/parallel_data_iterator.py configs/waymo/motion_mask_compuation/waymo_neural_reg.py --split ${i} > logs/${i}.log 2>&1 &
 done

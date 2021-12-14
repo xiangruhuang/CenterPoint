44daa8c1a5fa7bcf3eef543431935ec786930582
xiangruhuang <xiangruhuang816@gmail.com>
2021-12-14 04:11:09 +0000

    parallel

--- waymo_trace.sh
+++ waymo_trace.sh
@@ -6,5 +6,5 @@ mkdir -p logs
 
 for i in `seq ${start} ${end}`; do
   gpu_id=$((i % 8));
-  CUDA_VISIBLE_DEVICES=${gpu_id} OMP_NUM_THREADS=6 python tools/parallel_data_iterator.py configs/waymo/motion_mask_compuation/waymo_naive_obj_tracking.py --split ${i} --step 16 > logs/${i}.log 2>&1 &
+  CUDA_VISIBLE_DEVICES=${gpu_id} OMP_NUM_THREADS=6 python tools/parallel_data_iterator.py configs/waymo/motion_mask_compuation/waymo_naive_obj_tracking.py --split ${i} --step 16 --gpus 8 > logs/${i}.log 2>&1 &
 done

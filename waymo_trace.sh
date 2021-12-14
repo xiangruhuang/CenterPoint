db1d50b76fce0294e52c3519c082da464a4d9678
xiangruhuang <xiangruhuang816@gmail.com>
2021-12-13 23:53:19 -0500

    list

--- waymo_trace.sh
+++ waymo_trace.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-start=$1
-end=$2
+list_file=$1
 mkdir -p work_dirs/candidate_traces/
 mkdir -p logs
 
-for i in `seq ${start} ${end}`; do
-  gpu_id=$((i % 8));
-  CUDA_VISIBLE_DEVICES=${gpu_id} OMP_NUM_THREADS=6 python tools/parallel_data_iterator.py configs/waymo/motion_mask_compuation/waymo_naive_obj_tracking.py --split ${i} --step 16 --gpus 8 > logs/${i}.log 2>&1 &
+for line in `cat ${list_file}`; do
+  IFS=', ' read -r -a array <<< "$line"
+  split=$array[0];
+  gpus=$array[1];
+  gpu_id=$array[2];
+  CUDA_VISIBLE_DEVICES=${gpu_id} OMP_NUM_THREADS=6 python tools/parallel_data_iterator.py configs/waymo/motion_mask_compuation/waymo_naive_obj_tracking.py --split ${split} --gpus ${gpus} > logs/${split}.log 2>&1 &
 done

4764a637f93697b796fa2f45ade0b73e541b3208
xiangruhuang <xiangruhuang816@gmail.com>
2022-01-23 15:47:44 -0500

    point contrast

--- det3d/models/bbox_heads/__init__.py
+++ det3d/models/bbox_heads/__init__.py
@@ -1,4 +1,5 @@
 from .center_head import CenterHead
+from .point_contrast_head import PointContrastHead
 from .center_head_ssl import CenterHeadSSL
 
-__all__ = ["CenterHead", "CenterHeadSSL"]
+__all__ = ["CenterHead", "CenterHeadSSL", "PointContrastHead"]
